f9aba0f8b50dc33df8f1ae2a3c878285bc1c6e9a
Steven Hill <steven@stevenhill.co.za>
2023-07-05 09:45:41 +0200

    Added Components and styling

--- app/root.jsx
+++ app/root.jsx
@@ -30,6 +30,16 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <Outlet />
