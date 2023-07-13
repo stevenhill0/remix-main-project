02f5bbcc6bad71d894c2e5fcdb7e0b8d768f6fb4
Steven Hill <steven@stevenhill.co.za>
2023-07-13 11:09:09 +0200

    pathless shared styles

--- app/root.jsx
+++ app/root.jsx
@@ -1,5 +1,4 @@
 import { cssBundleHref } from '@remix-run/css-bundle';
-import MainHeader from '~/components/navigation/MainHeader';
 
 import {
   Links,
@@ -43,7 +42,6 @@ export default function App() {
         />
       </head>
       <body>
-        <MainHeader />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
